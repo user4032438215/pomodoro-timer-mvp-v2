1c39eb803ef253b12537a8fb8498ef288ea634c7
user4032438215 <sigonghuiren@gmail.com>
2025-12-20 21:59:19 +0900

    やったこと:機密情報は.envファイル+.gitignoreで管理。express,pgパッケージ,cors,dotenv 導入。サーバー起動テストOK

---
+++ src/backend/js/db.js
@@ -0,0 +1,26 @@
+//PostgreSQL接続設定（pgライブラリ）
+
+require("dotenv").config(); //.envファイルを読み込む
+
+const express = require("express");
+const app = express(); // Expressフレームワークを読み込んでアプリケーションを作成
+
+const cors = require("cors"); // CORSを許可するためのライブラリを読み込む
+
+const { Pool } = require("pg"); // PostgreSQL用ライブラリを読み込む
+
+//.envファイル+.gitignoreで管理
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pool;
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log("✅ 起動: http://localhost:" + PORT)); //サーバー起動テスト node db.js
+
